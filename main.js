799dbd0367e44873a4963b839672f3d60a57ccf8
Ivan Lutsyk <Vanb4ik@mail.ru>
2017-06-15 00:39:47 +0300

    2

--- main.js
+++ main.js
@@ -8,7 +8,9 @@
  GET /countries - countries list
 * */
     var $table=$("#users-table");
+
     var $form = $("form[name$='users-edit']");
+
     $("#create").click(function (e)//показати/сховати форму
     {
         e.preventDefault();
@@ -28,62 +30,75 @@
             /*console.log($btn);*/
             if ($($btn).attr("class")==="btn btn-save")//якщо клас btn-save пушимо на сервер і створюємо/редагуємо строку
             {
-                /*console.log("submit");*/
-                var toCreate =
-                    {
-                        /*id: this.elements.id.value,*/
-                        fullName:   $("#fullname")[0].value,
-                        birthday:   $("#birthday")[0].value,
-                        profession: $("#profession")[0].value,
-                        address:    $("#address")[0].value,
-                        country:    $("#country")[0].value,
-                        shortInfo:  $("#short-info")[0].value,
-                        fullInfo:   $("#full-info")[0].value
-                    };
 
-                var xhr = new XMLHttpRequest();
-                xhr.open(toCreate.id ? "put":"post","/user");//якщо є ід - то "put" інакше "post"
-                xhr.responseType= "json";
-                xhr.setRequestHeader("Content-Type","application/json");//кажемо серверу що передаємо йому джейсон
-                var data =JSON.stringify(toCreate); // тут перетворюємо джейсон в строку
-                xhr.send(data); //відпраляємо запит та передаємо тепер ту строку
-                xhr.onreadystatechange = function ()
+            if(   $("#fullname")[0].value
+                &&$("#profession")[0].value
+                &&$("#address")[0].value
+                &&$("#short-info")[0].value)
                 {
-                    if (this.readyState !==this.DONE)
+                    var toCreate =
+                        {
+                            id: $form.attr("value"),
+                            fullName:   $("#fullname")[0].value,
+                            birthday:   $("#birthday")[0].value,
+                            profession: $("#profession")[0].value,
+                            address:    $("#address")[0].value,
+                            country:    $("#country")[0].value,
+                            shortInfo:  $("#short-info")[0].value,
+                            fullInfo:   $("#full-info")[0].value
+                        };
+
+                    var xhr = new XMLHttpRequest();
+                    xhr.open(toCreate.id ? "put":"post","/user");//якщо є ід - то "put" інакше "post"
+                    xhr.responseType= "json";
+                    xhr.setRequestHeader("Content-Type","application/json");//кажемо серверу що передаємо йому джейсон
+                    var data =JSON.stringify(toCreate); // тут перетворюємо джейсон в строку
+                    xhr.send(data); //відпраляємо запит та передаємо тепер ту строку
+                    xhr.onreadystatechange = function ()
                     {
-                        return;
-                    }
-                    var todo = this.response;
-                    /*console.log(todo);*/
-                    var rowExistent = document.getElementById(todo.id);
-                    addRow(todo,$table,rowExistent);
-                    if (rowExistent)
-                    {
-                        rowExistent.parentElement.removeChild(rowExistent);
-                    }
-
-                    clearForm();//почистити поля форми
-                };
-
-
+                        if (this.readyState !==this.DONE)
+                        {
+                            return;
+                        }
+                        var todo = this.response;
+                        if ($form.attr("value"))
+                        {
+                           editRow($form.attr("value"));
+                        }
+                        else
+                        {
+                            addRow(todo,$table);
+                        }
+                        $form.attr("value","");
+                        $($form).toggleClass("users-edit-hidden");
+                        clearForm();//почистити поля форми
+                    };
+                }
             }
             else if ($($btn).attr("class")==="btn btn-cancel")
             {
                 $($form).toggleClass("users-edit-hidden");
-
+                clearForm();//почистити поля форми
             }
         }
-
     });
+    function editRow(id)//функція редагуання строки
+    {
+        var $idTr= $("#"+id)[0];
+        $idTr.querySelector(".fullName").innerText=   $("#fullname")[0].value;
+        $idTr.querySelector(".profession").innerText= $("#profession")[0].value;
+        $idTr.querySelector(".short-info").innerText= $("#short-info")[0].value;
+
+    }
     $.getJSON("/user", function (uList)//заповняє таблицю з сервера+додає кнопки управління
     {
         for (var i = 0; i < uList.length; i++)
         {
             var $tr=$("<tr></tr>").attr("id",uList[i].id).appendTo($table);
 
-            $("<td></td>").text(uList[i].fullName).appendTo($tr);
-            $("<td></td>").text(uList[i].profession).appendTo($tr);
-            $("<td></td>").text(uList[i].shortInfo).appendTo($tr);
+            $("<td></td>").addClass("fullName").text(uList[i].fullName).appendTo($tr);
+            $("<td></td>").addClass("profession").text(uList[i].profession).appendTo($tr);
+            $("<td></td>").addClass("short-info").text(uList[i].shortInfo).appendTo($tr);
             var $td=$("<td></td>");
             $("<button type='button'>Remove</button>")
                 .addClass("removeBtn")
@@ -104,7 +119,7 @@
     {
         for (var i = 0; i < uList.length; i++)
         {
-            $("<option></option>").text(uList[i]).appendTo($("#country"));
+            $("<option></option>").attr("value",i).text(uList[i]).appendTo($("#country"));
         }
     });
     $($table).click(function (e)//слухач на кнопки видалення/редагування
@@ -123,18 +138,32 @@
              else if ($($btn).attr("class")==="editBtn")
              {
                  $($form).toggleClass("users-edit-hidden",false);
-             /*setToEdit(id,form);*/
-
+                 setToEdit($id,$form);
              }
-
         }
     });
-    function addRow(todo,table,before)//ф-ія створення рядків в твблиці
+    function setToEdit(id,form)//функція заповнення форми табличними значеннями
+    {
+        form.attr("value",id);
+        $.getJSON("/user?id="+id, function (uList)//заповняє таблицю з сервера+додає кнопки управління
+        {
+            $("#fullname")[0].value=uList.fullName;
+            $("#birthday")[0].value=uList.birthday;
+            $("#profession")[0].value=uList.profession;
+            $("#address")[0].value=uList.address;
+            /*$("#country")[0].value=uList.country;*/       // тут ще не придумав
+            $("#short-info")[0].value=uList.shortInfo;
+            $("#full-info")[0].value=uList.fullInfo;
+        });
+
+
+    }
+    function addRow(todo,table)//ф-ія створення рядків в твблиці
     {
         var $tr=$("<tr></tr>").attr("id",todo.id).appendTo(table);
-        $("<td></td>").text($("#fullname")[0].value).appendTo($tr);
-        $("<td></td>").text($("#profession")[0].value).appendTo($tr);
-        $("<td></td>").text($("#short-info")[0].value).appendTo($tr);
+        $("<td></td>").addClass("fullname").text($("#fullname")[0].value).appendTo($tr);
+        $("<td></td>").addClass("profession").text($("#profession")[0].value).appendTo($tr);
+        $("<td></td>").addClass("short-info").text($("#short-info")[0].value).appendTo($tr);
         var $td=$("<td></td>");
         $("<button type='button'>Remove</button>")
          .addClass("removeBtn")
@@ -169,9 +198,7 @@
         $("#birthday")[0].value="";
         $("#profession")[0].value="";
         $("#address")[0].value="";
-        /*$("#country")[0].value;*/
         $("#short-info")[0].value="";
         $("#full-info")[0].value="";
     }
-
 })(jQuery);
